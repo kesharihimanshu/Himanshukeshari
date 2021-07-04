e59f3fe0f4f94b70ea4b92c743fc78a99a2162fd
kesharihimanshu <86609702+kesharihimanshu@users.noreply.github.com>
2021-07-04 18:51:07 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,86 @@
+
+$(function () {
+	
+	"use strict";
+	
+	/* Preloader
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	setTimeout(function () {
+		$('.loader_bg').fadeToggle();
+	}, 1500);
+		
+
+     /* Product slider 
+     -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+     // optional
+     $('#blogCarousel').carousel({
+        interval: 15000
+     });
+
+
+});
+
+
+
+$(document).ready(function() {
+ 
+  $(".owl-carousel").owlCarousel();
+ 
+});
+
+
+
+// owl-carousel
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 15,
+  nav: true,
+
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 3
+    },
+    1000: {
+      items: 3
+    }
+  }
+})
+
+
+
+
+// menu js
+
+// define all UI variable
+const navToggler = document.querySelector('.nav-toggler');
+const navMenu = document.querySelector('.site-navbar ul');
+const navLinks = document.querySelectorAll('.site-navbar a');
+
+// load all event listners
+allEventListners();
+
+// functions of all event listners
+function allEventListners() {
+  // toggler icon click event
+  navToggler.addEventListener('click', togglerClick);
+  // nav links click event
+  // navLinks.addEventListener('click', togglerClick);
+  // navLinks.forEach( elem => elem.addEventListener('click', navLinkClick));
+}
+
+// togglerClick function
+function togglerClick() {
+  navToggler.classList.toggle('toggler-open');
+  navMenu.classList.toggle('open');
+}
+
+// navLinkClick function
+function navLinkClick() {
+  if(navMenu.classList.contains('open')) {
+    navToggler.click();
+  }
+}
